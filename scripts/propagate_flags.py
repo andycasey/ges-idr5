2afec27cf35ceca98649c2e966be9d750b68e217
Andy Casey <andycasey@gmail.com>
2016-10-01 15:22:26 +0100

    Allow constraints for flag propagation

--- scripts/propagate_flags.py
+++ scripts/propagate_flags.py
@@ -25,7 +25,6 @@ with open("flags.yaml", "r") as fp:
 
 
 # Clear any previous propagations before starting.
-'''
 logger.info("Clearing previous propagations and setting all to have passed_quality_control = True")
 database.update(
     """ UPDATE  results
@@ -35,27 +34,68 @@ database.update(
                 propagated_tech = '',
                 propagated_peculi = '',
                 propagated_remark = '',
-                passed_quality_control = true;""")
-'''
+                passed_quality_control = true
+         WHERE  passed_quality_control = false;""")
 
-N_propagations = {}
-N_marked_as_poor_quality = {}
+database.connection.commit()
+
+# Identify spurious spectra and mark them as such.
+N_peculiar_spectra = {}
+peculiar_spectra_kwds = dict(
+    and_or="and", sigma_discrepant=3, teff_discrepant=250, logg_discrepant=0.25)
+
+for wg in (10, 11, 12, 13):
+
+    logger.info("Querying for peculiar spectra in WG{}".format(wg))
+
+    kwds = dict(wg=wg)
+    kwds.update(peculiar_spectra_kwds)
+
+    peculiar_spectrum_query = """
+        with t4 as (
+        select id, cname, filename, avg_filename_teff, avg_cname_teff, stddev_cname_teff, abs((avg_cname_teff - avg_filename_teff)/(0.00001 + stddev_cname_teff)) as abs_sigma_teff_discrepant, avg_filename_logg, avg_cname_logg, stddev_cname_logg, abs((avg_cname_logg - avg_filename_logg)/(0.00001 + stddev_cname_logg)) as abs_sigma_logg_discrepant FROM (with ar as (select distinct on (filename) id, cname, trim(filename) as filename, avg(teff) over w as avg_filename_teff, avg(logg) over w as avg_filename_logg from (with n as (select id from nodes where wg = {wg}) select distinct on (r.filename, r.node_id) r.id, r.cname, trim(r.filename) as filename, r.node_id, teff, logg from n, results as r where r.node_id = n.id and teff <> 'NaN' or logg <> 'NaN') t window w as (partition by filename)) select ar.id, ar.cname, ar.filename, ar.avg_filename_teff, avg(avg_filename_teff) over w2 as avg_cname_teff, stddev(avg_filename_teff) over w2 as stddev_cname_teff, ar.avg_filename_logg, avg(avg_filename_logg) over w2 as avg_cname_logg, stddev(avg_filename_logg) over w2 as stddev_cname_logg FROM ar window w2 as (partition by cname)) t3)
+        select * from t4 where (t4.abs_sigma_teff_discrepant > {sigma_discrepant} and t4.abs_sigma_teff_discrepant <> 'NaN' and abs(t4.avg_cname_teff - avg_filename_teff) >= {teff_discrepant}) {and_or} (t4.abs_sigma_logg_discrepant > {sigma_discrepant} and abs(t4.avg_cname_logg - t4.avg_filename_logg) >= {logg_discrepant} and t4.abs_sigma_logg_discrepant <> 'NaN') order by cname asc;""".format(
+            **kwds)
+
+    peculiar_spectra = database.retrieve_table(peculiar_spectrum_query)
+    N_peculiar_spectra[wg] = len(peculiar_spectra)
+
+    for row in peculiar_spectra:
+
+        filenames = row["filename"].strip().split("|")
+        logger.info("Propagating {}/{}/{}".format(
+            row["id"], row["cname"], row["filename"]))
+
+        n = 0
+        for filename in filenames:
+            n += database.update(
+                """ UPDATE results
+                       SET propagated_tech = '10106-{}-00-00-A',
+                           propagated_tech_from_result_id = '{}',
+                           passed_quality_control = false
+                     WHERE filename LIKE '%{}%';""".format(
+                        wg, int(row["id"]), filename))
+
+        if n > 0:
+            logger.info("--> affected {} results".format(n))
+
+database.connection.commit()
+
 
 # PROPAGATE BY SPECTRUM
-
-for flag in qc_flags["propagate_flags_by_spectrum"]:
-
-    N_propagations.setdefault(flag, 0)
-
+def propagate_by_spectrum(flag, constraint=None):
+    
+    constraint_str = "" if constraint is None else " AND {}".format(constraint)
     affected = database.retrieve_table(
         """ SELECT  id, TRIM(filename) AS filename, TRIM(tech) as tech
             FROM    results
-            WHERE   tech LIKE '%{}-%'
-        """.format(flag))
+            WHERE   tech LIKE '%{}-%' {}
+        """.format(flag, constraint_str))
     
     if affected is None:
-        continue
+        return 0
 
+    N = 0
     for row in affected:
     
         # Each row can have multiple TECH flags, so first identify the TECH flag
@@ -77,7 +117,7 @@ for flag in qc_flags["propagate_flags_by_spectrum"]:
         for j, filename in enumerate(filenames):
             if not filename: continue
 
-            N = database.update(
+            n = database.update(
                 """ UPDATE  results
                     SET     propagated_tech_from_result_id = '{}',
                             propagated_tech = '{}',
@@ -87,31 +127,48 @@ for flag in qc_flags["propagate_flags_by_spectrum"]:
                 """.format(
                     int(row["id"]), matched_tech_flag, filename))
             
-            N_propagations[flag] += N
+            N += n
 
-            if N > 0:
+            if n > 0:
                 logger.info("Propagated ({}/{}/{}) to {} other entries".format(
-                    row["id"], matched_tech_flag, filename, N))    
-
+                    row["id"], matched_tech_flag, filename, n))    
 
         database.connection.commit()
 
+    return N
+
+
+# PROPAGATE BY SPECTRUM
+N_propagations = {}
+
+for key, value in qc_flags["propagate_flags_by_spectrum"].items():
+    if key == "no_constraint":
+        for flag in value:
+            N_propagations.setdefault(flag, 0)
+            N_propagations[flag] += propagate_by_spectrum(flag, None)
+    
+    else:
+        flag = key
+        constraint = value.get("constraint", None)
+        N_propagations.setdefault(flag, 0)
+        N_propagations[flag] += propagate_by_spectrum(flag, constraint)
+
+
 
 # PROPAGATE BY CNAME
+def propagate_by_cname(flag, constraint=None):
 
-for flag in qc_flags["propagate_flags_by_cname"]:
-
-    N_propagations.setdefault(flag, 0)
-
+    constraint_str = "" if constraint is None else " AND {}".format(constraint)
     affected = database.retrieve_table(
         """ SELECT  id, cname, TRIM(tech) as tech
               FROM  results
-             WHERE  tech LIKE '%{}-%'
-        """.format(flag))
+             WHERE  tech LIKE '%{}-%' {}
+        """.format(flag, constraint_str))
 
     if affected is None:
-        continue
+        return 0
 
+    N = 0
     for row in affected:
         # Each row can have multiple TECH flags, so first identify the TECH flag
         # that PostgreSQL matched on.
@@ -127,7 +184,7 @@ for flag in qc_flags["propagate_flags_by_cname"]:
                     flag, row["tech"].strip()))
 
         # Update other results matching this CNAME.
-        N = database.update(
+        n = database.update(
             """ UPDATE  results
                    SET  propagated_tech_from_result_id = '{}',
                         propagated_tech = '{}',
@@ -135,29 +192,69 @@ for flag in qc_flags["propagate_flags_by_cname"]:
                  WHERE  cname = '{}'
                    AND  passed_quality_control = true;
             """.format(int(row["id"]), matched_tech_flag, row["cname"]))
-        N_propagations[flag] += N
+        N += n
 
-        if N > 0:
+        if n > 0:
             logger.info("Propagated ({}/{}/{}) to {} other entries".format(
-                row["id"], matched_tech_flag, row["cname"], N))
+                row["id"], matched_tech_flag, row["cname"], n))
 
         database.connection.commit()
 
+    return N
+
+
+# PROPAGATE BY CNAME
+for key, value in qc_flags["propagate_flags_by_cname"].items():
+
+    if key == "no_constraint":
+        for flag in value:
+            N_propagations.setdefault(flag, 0)
+            N_propagations[flag] += propagate_by_cname(flag, None)
+
+    else:
+        flag = key
+        constraint = value.get("constraint", None)
+        N_propagations.setdefault(flag, 0)
+        N_propagations[flag] += propagate_by_cname(flag, constraint)
+
 
 # NODE-SPECIFIC FLAGS
+N_marked_as_poor_quality = {}
+for key, value in qc_flags["node_specific_flags"].items():
 
-for node_specific_flag in qc_flags["node_specific_flags"]:
+    if key == "no_constraint":
+        for flag in value:
+            N_marked_as_poor_quality.setdefault(flag, 0)
+            N = database.update(
+                """ UPDATE  results
+                       SET  passed_quality_control = false
+                     WHERE  tech LIKE '%{}-%'
+                       AND  passed_quality_control = true;
+                """.format(flag))
+            N_marked_as_poor_quality[flag] += N
 
-    N = database.update(
-        """ UPDATE  results
-               SET  passed_quality_control = false
-             WHERE  tech LIKE '%{}-%'
-               AND  passed_quality_control = true;
-        """.format(node_specific_flag))
-    N_marked_as_poor_quality[node_specific_flag] = N
+            if N > 0:
+                logger.info(
+                    "Marked {} results as poor quality due to matching flag {}"\
+                    .format(N, flag))
+    else:
+        flag = key
+        constraint = value.get("constraint", None)
+        constraint_str = "" if constraint is None else " AND {}".format(constraint)
+        N_marked_as_poor_quality.setdefault(flag, 0)
 
-    if N > 0:
-        logger.info("Marked {} results as poor quality due to matching flag {}"\
-            .format(N, node_specific_flag))
+        N = database.update(
+            """ UPDATE  results
+                   SET  passed_quality_control = false
+                 WHERE  tech LIKE '%{}-%'
+                   AND  passed_quality_control = true
+                   {};
+            """.format(flag, constraint_str))
+        N_marked_as_poor_quality[flag] += N
 
-database.connection.commit()
+        if N > 0:
+            logger.info(
+                "Marked {} results as poor quality due to matching flag {} "\
+                "and constraint {}".format(N, flag, constraint))
+
+    database.connection.commit()
